3170140a1aafde0d2294b2276b2691a47d52ab93
WhalenSITHS <michael.whalen@sitechhs.com>
2020-01-08 11:16:53 -0500

    coonecto DB

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect("paste something here", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true
-});
+mongoose.connect(
+  "mongodb+srv://mike:siths1234@cluster0-ls6pq.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+  }
+);
