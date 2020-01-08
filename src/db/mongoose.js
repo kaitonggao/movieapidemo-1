e732e34974b0458fa68d4a0d3b0f72e6113a3a37
WhalenSITHS <michael.whalen@sitechhs.com>
2020-01-08 11:34:43 -0500

    dfgsdfgsdf

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://mike:siths1234@cluster0-ls6pq.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb+srv://mike:siths124@cluster0-ls6pq.mongodb.net/test?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
---
+++ src/routers/movies.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const Movie = require("../models/movie");
+const router = new express.Router();
+
+router.post("/movies", async (req, res) => {
+  try {
+    const movie = new Movie(req.body);
+    await movie.save();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
