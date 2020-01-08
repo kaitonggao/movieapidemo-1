fe90df593fb3d80639fa9f1c05e2d43327f46d13
WhalenSITHS <michael.whalen@sitechhs.com>
2020-01-08 11:46:37 -0500

    dfghdfghdfgh

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -6,6 +6,7 @@ router.post("/movies", async (req, res) => {
   try {
     const movie = new Movie(req.body);
     await movie.save();
+    res.send(movie);
   } catch (error) {
     res.status(500).send(error);
   }
