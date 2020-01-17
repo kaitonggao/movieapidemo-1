7ddc7b586f0c57a01f05058da05c84cf2da2bf0b
WhalenSITHS <michael.whalen@sitechhs.com>
2020-01-17 11:45:11 -0500

    Update user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -20,7 +20,9 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateToken();
+
+    res.send({ user, token });
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
