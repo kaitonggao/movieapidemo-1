d499c6e228bd0eda07210776f288dd5bf2903bfb
kaitonggao <50878288+kaitonggao@users.noreply.github.com>
2020-02-05 11:46:17 -0500

    fix auth

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://mike:siths1234@cluster0-ls6pq.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb+srv://harmony:admin@cluster0-9f9v1.mongodb.net/test?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
